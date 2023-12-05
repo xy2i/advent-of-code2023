dbd2da0d5f2a5bf911b69ad7e6da7da73faa13e8
xy2 <hugo@xy2.dev>
2023-12-05 19:22:29 +0100

    a

--- src/day05.rs
+++ src/day05.rs
@@ -1,20 +1,78 @@
-use std::cmp;
+use std::{cmp, collections::BTreeMap, mem::swap, ops::Range};
 
 use aoc_runner_derive::{aoc, aoc_generator};
-use rangemap::RangeMap;
+
+use crate::numbers_space;
 
 #[derive(Debug)]
-struct TextMapEntry {
-    dest_range_start: u32,
-    source_range_start: u32,
-    range_len: u32,
+struct MapEntry {
+    dst: u32,
+    src: u32,
+    len: u32,
 }
 
-type TextMap = Vec<TextMapEntry>;
+impl MapEntry {
+    fn parse(input: &str) -> Self {
+        let mut iter = numbers_space(input);
+        let dst = iter.next().unwrap();
+        let src = iter.next().unwrap();
+        let len = iter.next().unwrap();
+        Self { dst, src, len }
+    }
+}
 
-struct Input {
+#[derive(Debug, Clone)]
+struct Map {
+    set: BTreeMap<u32, u32>,
+}
+
+impl Default for Map {
+    fn default() -> Self {
+        let mut set = BTreeMap::new();
+        set.insert(0, 0);
+        Self { set }
+    }
+}
+
+impl Map {
+    fn map(&self, x: u32) -> u32 {
+        let (src, dst) = self.set.range(..=x).next_back().unwrap();
+        let diff = x - src;
+        dst + diff
+    }
+
+    fn map_ranges(&self, input: &mut Vec<Range<u32>>, output: &mut Vec<Range<u32>>) {
+        for mut r in input.drain(..) {
+            while !r.is_empty() {
+                println!("{r:?}");
+                let (&src1, &dst1) = self.set.range(..=r.start).next_back().unwrap();
+                let output2 = self.set.range(r.start + 1..r.end).next();
+
+                let diff = dst1.wrapping_sub(src1);
+                let start = r.start.wrapping_add(diff);
+
+                let end = match output2 {
+                    None => r.end,
+                    Some((&src2, _)) => src2,
+                };
+
+                r.start = end;
+                let end = end.wrapping_add(diff);
+                output.push(start..end);
+            }
+        }
+    }
+
+    fn push(&mut self, entry: MapEntry) {
+        let end = entry.src.saturating_add(entry.len);
+        self.set.entry(end).or_insert(end);
+        self.set.insert(entry.src, entry.dst);
+    }
+}
+
+pub struct Input {
     seeds: Vec<u32>,
-    textmaps: Vec<TextMap>,
+    maps: Vec<Map>,
 }
 
 #[aoc_generator(day5)]
@@ -24,91 +82,53 @@ pub fn parse(input: &str) -> Input {
 
     let seeds: Vec<u32> = crate::numbers_space(input.next().unwrap()).collect();
 
-    let mut textmaps = vec![];
+    let mut maps = vec![];
     for line in input {
         if line.trim().is_empty() {
             continue;
         }
         let first = line.chars().nth(0).unwrap();
         if first < '0' || first > '9' {
-            textmaps.push(vec![]);
+            maps.push(Map::default());
             continue;
         }
 
-        let current_textmap = textmaps.iter_mut().last().unwrap();
-        let mut iter = crate::numbers_space(line);
-        current_textmap.push(TextMapEntry {
-            dest_range_start: iter.next().unwrap(),
-            source_range_start: iter.next().unwrap(),
-            range_len: iter.next().unwrap(),
-        })
+        let current_textmap = maps.iter_mut().last().unwrap();
+        current_textmap.push(MapEntry::parse(line));
     }
 
-    Input { seeds, textmaps }
+    Input { seeds, maps }
 }
 
 #[aoc(day5, part1)]
-fn part1(input: &Input) -> i64 {
-    let rangemaps = input
-        .textmaps
+fn part1(input: &Input) -> u32 {
+    input
+        .seeds
         .iter()
-        .map(|textmap| {
-            let mut map = RangeMap::new();
-            for entry in textmap {
-                map.insert(
-                    entry.source_range_start as i64
-                        ..entry.source_range_start as i64 + entry.range_len as i64,
-                    entry.dest_range_start as i64 - entry.source_range_start as i64,
-                )
-            }
-            map
-        })
-        .collect::<Vec<_>>();
-
-    let mut low = i64::MAX;
-    for &number in &input.seeds {
-        let mut number = number as i64;
-        for range_map in &rangemaps {
-            let &offset = range_map.get(&number).unwrap_or(&0);
-            number += offset;
-        }
-        low = cmp::min(low, number);
-    }
-    low
+        .map(|&seed| input.maps.iter().fold(seed, |acc, map| map.map(acc)))
+        .min()
+        .unwrap()
 }
 
 #[aoc(day5, part2)]
-fn part2(input: &Input) -> i64 {
-    let rangemaps = input
-        .textmaps
-        .iter()
-        .map(|textmap| {
-            let mut map = RangeMap::new();
-            for entry in textmap {
-                map.insert(
-                    entry.source_range_start as i64
-                        ..entry.source_range_start as i64 + entry.range_len as i64,
-                    entry.dest_range_start as i64 - entry.source_range_start as i64,
-                )
-            }
-            map
-        })
+fn part2(input: &Input) -> u32 {
+    let mut ranges1 = input
+        .seeds
+        .chunks(2)
+        .map(|chunk| chunk[0]..chunk[0] + chunk[1])
         .collect::<Vec<_>>();
+    let mut ranges2 = Vec::with_capacity(ranges1.len());
 
-    let mut low = i64::MAX;
-    for pair in input.seeds.chunks(2) {
-        let number = pair[0] as i64;
-        let len = pair[1] as i64;
+    let map = &input.maps;
+    map[0].map_ranges(&mut ranges1, &mut ranges2);
+    map[1].map_ranges(&mut ranges2, &mut ranges1);
+    map[2].map_ranges(&mut ranges1, &mut ranges2);
+    map[3].map_ranges(&mut ranges2, &mut ranges1);
+    map[4].map_ranges(&mut ranges1, &mut ranges2);
+    map[5].map_ranges(&mut ranges2, &mut ranges1);
+    map[6].map_ranges(&mut ranges1, &mut ranges2);
 
-        for mut number in number..number + len {
-            for range_map in &rangemaps {
-                let &offset = range_map.get(&number).unwrap_or(&0);
-                number += offset;
-            }
-            low = cmp::min(low, number);
-        }
-    }
-    low
+    ranges2.into_iter().map(|r| r.start).min().unwrap()
 }
 
 #[test]
